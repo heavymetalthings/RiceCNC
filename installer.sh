78af3c65d51486177f226da7c996d29d4ed753ee
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-08-08 19:52:38 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,11 @@
+# installing the bare minimum (which is bloat for some people)
+
+apt install xorg git screen build-essentials neofetch btop nano
+
+# cloning dwm, st and 
+
+mkdir dev 
+cd dev
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
 
