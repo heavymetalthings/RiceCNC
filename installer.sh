ec5453f7bac3a522e20f503fe2bde7dea892c1c5
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-08-09 00:21:04 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -43,6 +43,6 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 
-sudo apt update && sudo apt install codium
+sudo apt update && sudo apt install codium -y
 
-sudo apt install linuxcnc-uspace linuxcnc-uspace-dev
+sudo apt install linuxcnc-uspace linuxcnc-uspace-dev -y
