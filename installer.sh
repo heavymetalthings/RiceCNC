2f3896843a56ed112b077e110fbaf3b6bebf739d
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-08-09 15:32:09 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -62,9 +62,9 @@ sudo apt update -y
 sudo apt install brave-browser-nightly -y
 
 #remove the default xorg session and replace it with the dwm session file
-cd /usr/share/xsessions/
-sudo rm -r lightdm-xsession.desktop
+#cd /usr/share/xsessions/
+#sudo rm -r lightdm-xsession.desktop
 
 cd 
-cd RiceCNC
+cd RiceCNC/
 sudo cp lightdm-xsession.desktop /usr/share/xsessions/
