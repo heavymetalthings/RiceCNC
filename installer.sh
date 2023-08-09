4d1080082bf7648d2527614fe4c5762cdedc1751
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-08-09 15:36:02 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -54,19 +54,21 @@ sudo apt install lightdm -y
 #remove the default xorg session and replace it with the dwm session file
 #cd /usr/share/xsessions/
 #sudo rm -r lightdm-xsession.desktop
-cd 
+ls
+cd
+ls
 cd RiceCNC/
 sudo cp lightdm-xsession.desktop /usr/share/xsessions/
 
 #install brave
-sudo apt install curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
+#sudo apt install curl -y
+#sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list 
+#echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list 
 
-sudo apt update -y
+#sudo apt update -y
 
-sudo apt install brave-browser-nightly -y
+#sudo apt install brave-browser-nightly -y
 
 
 
