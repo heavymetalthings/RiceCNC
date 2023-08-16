c96965ec8e93c6b5ffeb7347e3c13bd69e792eb7
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-08-16 02:20:53 +0200

    Update installer_full.sh

--- installer_full.sh
+++ installer_full.sh
@@ -1,36 +1,35 @@
 #chmod o+x /root/
-
 cd
-
 sudo apt install xorg screen build-essential -y
-
 sudo apt install libx11-dev -y
 sudo apt install libxft-dev -y
 sudo apt install libxinerama-dev -y
-
 mkdir dev 
 cd dev
 git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/st
 git clone https://git.suckless.org/dmenu
-
-#compiling the 3 packages
 cd dwm
 make
 sudo make clean install
 cd 
-
-#cd dev
-#cd st
-#make
-#sudo make clean install 
-#cd 
-
 cd dev
 cd dmenu
 make
 sudo make clean install 
 cd 
+sudo apt install linuxcnc-uspace linuxcnc-uspace-dev -y
+sudo apt install lightdm -y
+cd ..
+cd home
+cd */
+ls
+cd RiceCNC/
+sudo cp lightdm-xsession.desktop /usr/share/xsessions/
+
+
+
+
+sudo apt install neofetch btop nano -y
 
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
@@ -41,21 +40,6 @@ echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https:/
 
 sudo apt update && sudo apt install codium -y
 
-sudo apt install linuxcnc-uspace linuxcnc-uspace-dev -y
-
-sudo apt install lightdm -y
-
-#remove the default xorg session and replace it with the dwm session file
-#cd /usr/share/xsessions/
-#sudo rm -r lightdm-xsession.desktop
-cd ..
-cd home
-cd */
-ls
-cd RiceCNC/
-sudo cp lightdm-xsession.desktop /usr/share/xsessions/
-
-install brave
 sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
 
@@ -64,20 +48,5 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.g
 sudo apt update -y
 
 sudo apt install brave-browser-nightly -y
-
-
-#cd ..
-#cd /home
-#cd */
-#nomUtilisateur=${PWD##*/} 
-
-#access and modification of the var in the lightdm config file
-#cd /etc/lightdm/
-#tempVar="autologin-user = ${nomUtilisateur}"
-#sudo sed -i 's/# autologin-user = User to log in with by default (overrides autologin-guest)/$tempVar/g' lightdm.conf
-#sudo sed -i 's/# autologin-user-timeout = Number of seconds to wait before loading default user/autologin-user-timeout = 0/g' lightdm.conf
-
-
-sudo apt install neofetch btop nano -y
 
 #sudo reboot 
